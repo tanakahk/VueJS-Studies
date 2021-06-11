ae661664a6884328fed9f481d8663aa1d3208f9e
Tanaka <74397585+tanakahk@users.noreply.github.com>
2021-06-11 03:42:53 -0300

    Aula 2 finalizada

---
+++ src/components/Card.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card">
+
+    <div>
+      {{a}}
+
+      <button ref="el" @click="clickHandler">++</button>
+    </div>
+
+    <div class="card-title">
+      {{title}}
+    </div>
+    <div>
+      {{body}}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: { type: String, default: '' },
+    body: { type: String, default: '' },
+  },
+
+  setup() {
+    const a = ref(0);
+    const el = ref();
+
+    const clickHandler = () => {
+      // console.log(e);
+      // console.log(a.value);
+      console.log(el.value);
+      a.value++;
+      el.value.innerHTML = 'hello';
+    };
+
+    return {
+      a,
+      el,
+      clickHandler,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.card{
+  border: 1px solid black;
+  padding: 5px;
+  width: 200px;
+  height: 200px;
+  margin: 5px;
+}
+
+.card-title{
+  border-bottom: 1px solid black
+}
+</style>
---
+++ src/components/__Model.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Componente</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+</style>
