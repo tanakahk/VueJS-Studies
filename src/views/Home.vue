1d7c59c92d96465743bcc1587fda22a01131ca52
Tanaka <74397585+tanakahk@users.noreply.github.com>
2021-07-08 00:57:38 -0300

    revisão + filtro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <div>Pokemons disponiveis</div>
+
+    <select v-model="category">
+      <option value="">Sem filtro</option>
+      <option v-for="cat in categories" :key="cat" :value="cat">
+        {{ cat }}
+      </option>
+    </select>
+
     <div style="display: flex; flex-wrap: wrap">
       <div v-for="p in pokemons" :key="p.id">
         <card :pokemon="p" :images="getImages(p)" :types="p.types" />
@@ -18,7 +26,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { defineComponent, computed, ref } from 'vue';
 import Card from '@/components/Card.vue';
 import useCards, { Pokemon } from '@/modules/cards';
 
@@ -29,9 +37,22 @@ export default defineComponent({
 
   setup() {
     const cards = useCards();
+    const category = ref();
 
-    const pokemons = computed(() => cards.state.pokemons);
     const myPokemons = computed(() => cards.state.myPokemons);
+    const categories = computed(() => cards.state.categories);
+    const pokemons = computed(() => {
+      if (category.value) {
+        return cards.state.pokemons.filter((p) => {
+          const exists = p.types.find(
+            (type) => type.type.name === category.value,
+          );
+          console.log('p', exists, p.types);
+          return exists;
+        });
+      }
+      return cards.state.pokemons;
+    });
 
     const getImages = (pokemon: Pokemon) => [
       pokemon.sprites.frontDefault,
@@ -48,8 +69,10 @@ export default defineComponent({
     return {
       pokemons,
       myPokemons,
+      categories,
       getImages,
       loadMore,
+      category,
     };
   },
 });
